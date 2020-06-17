b2e955eee80432bafd7cbf40879a2e6c7d03439a
Nagy-Bana Éva <nagybanaeva@gmail.com>
2020-06-17 22:25:38 +0200

    add files

---
+++ script.js
@@ -0,0 +1,15 @@
+var solarSystem = {
+	'Mercury': 'bolygó', 
+	'Venus': 'bolygó', 
+	'Earth': 'bolygó', 
+	'Mars': 'bolygó', 
+	'Jupiter': 'bolygó', 
+	'Saturn': 'bolygó', 
+	'Uranus': 'bolygó', 
+	'Neptune': 'bolygó', 
+	'Pluto': 'bolygó', 
+	'Ceres': 'törpebolygó', 
+	'Makemake': 'törpebolygó', 
+	'Haumea': 'törpebolygó', 
+	'Eris': 'törpebolygó'
+};
